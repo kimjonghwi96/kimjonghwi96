ca7ab734d661285fbeed131cf15f4765670cbcb7
UserK <UserK@Win10-01>
2024-02-05 18:19:43 +0900

    알고리즘연습

--- test/src/main/java/test/main3.java
+++ test/src/main/java/test/main3.java
@@ -11,42 +11,119 @@ public class main3 {
 
 	public static void main(String[] args) {
 
-//		solution 실험
-//		Solution sol1 = new Solution();
-//		sol1.solution("1100111");
+/////////////////////////////////////////////////////////////////////////////////
+//		이진변환
+		
+//		String s1 = "11011001101";
+//		int[] answer = new int[2];
+//		int cnt0 = 0;
+//		int cntI = 0;
+//
+//		while (s1.equals("1") != true) {
+//			String s1_minus0 = "";
+//			int s2 = 0;
+//			String s3 = "";
+//
+//			for (int i = 0; i < s1.length(); i++) {
+//				if (s1.substring(i, i + 1).equals("1")) {
+//					s1_minus0 = s1_minus0 + 1;
+//				}
+//			}
+//			System.out.println("s1 : " + s1);
+//			System.out.println("s1_minus0 : " + s1_minus0);
+//			s2 = s1_minus0.length();
+//			System.out.println("s2 : " + s2);
+//			cnt0 = cnt0 + s1.length() - s1_minus0.length();
+//			System.out.println("cnt0 : " + cnt0);
+//
+//			System.out.println("===========================");
+//
+//			while (true) {
+//				System.out.println("s2 : " + s2);
+//				System.out.println("s3 : " + s3);
+//				if (s2 == 1) {
+//					s3 = 1 + s3;
+//					break;
+//				} else if (s2 % 2 == 1) {
+//					s3 = "1" + s3;
+//				} else {
+//					s3 = "0" + s3;
+//				}
+//				s2 = s2 / 2;
+//			}
+//			System.out.println("s3결과 : " + s3);
+//			s1 = s3;
+//			System.out.println("s1재설정 : " + s1);
+//			cntI = cntI+1;
+//		}
+//		answer[0] = cntI;
+//		answer[1] = cnt0;
+//		System.out.println("답 : " + answer[0] + ", " + answer[1]);
 
-		String s1 = "101010";
-		int[] answer = new int[2];
-		String s1_minus0 = "";
-		int s2 = 0;
-		int cntI = 0;
-		int cnt0 = 0;
-
-		while (s1.equals("1") != true) {
-			for (int i = 0; i < s1.length(); i++) {
-				if (s1.substring(i, i + 1).equals("1")) {
-					s1_minus0 = s1_minus0 + 1;
+/////////////////////////////////////////////////////////////////////////////////
+//		연속된 숫자를 더해서 int1을 만들수 있는 경우의 수
+//		int int1 = 1;
+//		int cnt = 0;
+//		for (int i=1;i<=int1/2;i++) {
+//			int sum=0;
+//			for (int j=0;j<int1;j++) {
+//				int i2=i+j;
+//				sum=sum+i2;
+//				if(sum>int1) {
+//					break;
+//				} else if(sum==int1) {
+//					cnt = cnt+1;
+//					System.out.println("i : " + i);
+//					break;
+//				}
+//			}
+//		}
+//		cnt = cnt+1;
+//		System.out.println(cnt);
+////////////////////////////////////////////////////////////////////
+//		큰수(이진법으로 1갯수 같은) 찾기 효율실패
+//		int int1 = 78;
+//		int answer = 0;
+//		int length1 = Integer.toBinaryString(int1).replace("0","").length();
+//		System.out.println(length1);
+//		for(int i=int1+1;i<=2*int1;i++) {
+//			int length2 = Integer.toBinaryString(i).replace("0","").length();
+//			if(length2 == length1) {
+//				answer=i;
+//				break;
+//			}
+//		}
+//		System.out.println(answer);
+////////////////////////////
+//		큰수 찾기
+		int int1=100;
+		String str1="0"+Integer.toBinaryString(int1);
+		String str2="";
+		String str3="";
+		int answer = 0;
+		System.out.println(str1);
+		for(int i=1;i<str1.length()-2;i++) {
+			if(str1.substring(str1.length()-i-2,str1.length()-i).equals("01")) {
+				str2=str1.substring(0,str1.length()-i-2)+"10";
+				int len1 = Integer.toString(Integer.parseInt(str1)).replace("0", "").length();
+				int len2 = Integer.toString(Integer.parseInt(str2)).replace("0", "").length();
+				for(int j=0;j<len1-len2;j++) {
+					str3 = str3 + "1";
 				}
-			}
-			cnt0 = cnt0 + s1.length() - s1_minus0.length();
-			s2 = s1_minus0.length();
-			s1 = "";
-			while (s2 > 1) {
-				if (s2 / 2 >= 1) {
-					s1 = s1 + 1;
-				} else {
-					s1 = s1 + 0;
+				for(int j=0;j<i-len1+len2;j++) {
+					str3 = "0" + str3;
 				}
-				s2 = s2 - s2 / 2;
-//				System.out.println(s2);
+				str2 = str2 + str3;
 			}
-
-			cntI = cntI + 1;
 		}
-		System.out.println(s1);
-		answer[0] = cntI;
-		answer[1] = cnt0;
-		System.out.println(answer);
+		answer = Integer.parseInt(str2, 2);
 
+//		System.out.println("len1 : " + len1 + ", len2 : " + len2);
+		System.out.println("int1 : " + Integer.toBinaryString(int1));
+		System.out.println("str2 : " + Integer.parseInt(str2));
+		
+		System.out.println("answer : " + answer);
+
+		
 	}
 }
